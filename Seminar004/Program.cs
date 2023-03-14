39bf5f65e1be5664ffa4a12e4beed59e36ad45f2
Sergey <dsa1972@mail.ru>
2023-03-14 21:21:55 +0300

    add Seminar004

--- Seminar004/Program.cs
+++ Seminar004/Program.cs
@@ -81,38 +81,38 @@
 
 // Еще один вариант.
 
-int Prompt(string message)
-{
-  System.Console.Write(message);
-  string ReadInput = System.Console.ReadLine();
-  int result = int.Parse(ReadInput);
-  return result;
-}
+// int Prompt(string message)
+// {
+//   System.Console.Write(message);
+//   string ReadInput = System.Console.ReadLine();
+//   int result = int.Parse(ReadInput);
+//   return result;
+// }
 
-int[] GenerateArray(int Length, int minValue, int maxValue)
-{
-  int[] array;
-  array = new int [Length];
-Random random = new Random();
-for (int i = 0; i < Length; i++)
-{
-array[i] = random.Next(minValue, maxValue + 1);
-}
-return array;
-}
+// int[] GenerateArray(int Length, int minValue, int maxValue)
+// {
+//   int[] array;
+//   array = new int [Length];
+// Random random = new Random();
+// for (int i = 0; i < Length; i++)
+// {
+// array[i] = random.Next(minValue, maxValue + 1);
+// }
+// return array;
+// }
 
-void PrintArray(int[] array)
-{
-  System.Console.Write("[");
-  for (int i = 0; i < array.Length -1; i++)
-  {
-    System.Console.Write($"{array[i]}, ");
-  }
-  System.Console.Write($"{array[array.Length - 1]}");
-  System.Console.WriteLine("]");
-}
-int length = Prompt("Длина массива: ");
-int min = Prompt("Начальное значение, для диапазона случайного числа: ");
-int max = Prompt("Конечное значение, для диапазона случайного числа: ");
-int[] array = GenerateArray(length, min, max);
-PrintArray(array);
+// void PrintArray(int[] array)
+// {
+//   System.Console.Write("[");
+//   for (int i = 0; i < array.Length -1; i++)
+//   {
+//     System.Console.Write($"{array[i]}, ");
+//   }
+//   System.Console.Write($"{array[array.Length - 1]}");
+//   System.Console.WriteLine("]");
+// }
+// int length = Prompt("Длина массива: ");
+// int min = Prompt("Начальное значение, для диапазона случайного числа: ");
+// int max = Prompt("Конечное значение, для диапазона случайного числа: ");
+// int[] array = GenerateArray(length, min, max);
+// PrintArray(array);
