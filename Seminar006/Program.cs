4c270f0de3af9151d36d3665b9f5981b39363778
Sergey <dsa1972@mail.ru>
2023-03-20 22:18:12 +0300

    add Seminar006.dz

--- Seminar006/Program.cs
+++ Seminar006/Program.cs
@@ -35,19 +35,19 @@
 // Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
 
-System.Console.WriteLine("Введите длину стороны АБ: ");
-int sideAB = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите длину стороны БС: ");
-int sideBC = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите длину стороны АС: ");
-int sideAC = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите длину стороны АБ: ");
+// int sideAB = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите длину стороны БС: ");
+// int sideBC = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите длину стороны АС: ");
+// int sideAC = Convert.ToInt32(Console.ReadLine());
 
-if (sideAB + sideBC > sideAC && sideAC + sideBC > sideAB && sideAB + sideAC > sideBC)
-  Console.WriteLine($"Треугольник со сторонами {sideAB}, {sideBC}, {sideAC} может быть");
-else
-{
-  Console.WriteLine($"Треугольника со сторонами {sideAB}, {sideBC}, {sideAC} не может быть");
-}
+// if (sideAB + sideBC > sideAC && sideAC + sideBC > sideAB && sideAB + sideAC > sideBC)
+//   Console.WriteLine($"Треугольник со сторонами {sideAB}, {sideBC}, {sideAC} может быть");
+// else
+// {
+//   Console.WriteLine($"Треугольника со сторонами {sideAB}, {sideBC}, {sideAC} не может быть");
+// }
 
 
 // Задача 42: Напишите программу, которая будет 
@@ -55,3 +55,84 @@ else
 // 45 -> 101101
 // 3  -> 11
 // 2  -> 10
+
+//Вариант №1
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int end = 0;
+// int d = 1;
+// while (number > 0)
+// {
+//   end = end+number%2*d;
+//   number /=2;
+//   d *=10; 
+// }
+// Console.WriteLine(end);
+
+// Вариант №2
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// void PrintBinary(int number)
+// {
+//   if (number <= 0)
+//   {
+//     return;
+//   }
+//   PrintBinary(number / 2);
+//   Console.Write(number % 2);
+// }
+// PrintBinary(number);
+
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int first = 0;
+// int second = 1;
+// int fib = 0;
+// Console.Write("0 1 ");
+// for (int i = 2; i < number; i++)
+// {
+//   fib = first + second;
+//   first = second;
+//   second = fib;
+//   Console.Write($"{fib} ");
+// }
+
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+int[] FillArray(int size)
+{
+  int[] array = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = rnd.Next(0, 11);
+  }
+  return array;
+}
+
+int[] CopyArray(int[] arr, int size)
+{
+  int[] arrayNew = new int[size];
+  for(int i = 0; i < arrayNew.Length; i++)
+  {
+    arrayNew[i] = arr[i];
+  }
+ return arrayNew;
+}
+
+System.Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = FillArray(size);
+int[] copy = CopyArray(arr, size);
+System.Console.WriteLine($"[{string.Join(", ", arr)}]");
+System.Console.WriteLine($"[{string.Join(", ", copy)}]");
